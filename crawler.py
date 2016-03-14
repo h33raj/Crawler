61bf708aaa9875ee1968f83bf0ea45763776ceac
heeraj123 <heeraj123@gmail.com>
2016-03-14 07:36:57 +0530

    Change

--- crawler.py
+++ crawler.py
@@ -1,11 +1,8 @@
 import argparse
 import os
 
-print "-"*140
-print "-"*140
-
 parser = argparse.ArgumentParser(description='Information Gathering software')
-parser.add_argument('-u', '-url', type=str, help='Target URL')
+parser.add_argument('-u', '--url', type=str, help='Target URL')
 args = parser.parse_args()
 
 if not args.u:
